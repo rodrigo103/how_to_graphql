b334cacaa4ced9931a1f35888e2b2b1e7fbb8024
Rodrigo <rvidela@outlook.com>
2019-07-23 18:05:13 -0300

    Upgraded to the next version, from the link: https://www.howtographql.com/graphql-js/3-a-simple-mutation/ still the last exercise is left to do.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }
+];
 
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
-
-// 1
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    // 2
-    feed: () => links,
+    feed: () => links
   },
-  // 3
   Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    }
   }
-}
-
+};
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 });
+
 server.start(() => console.log(`Server is running on http://localhost:4000`));
